39ac552624a2cb552b82d6d2305ae7a345212c77
ASchwad <a.schoenenwald@gmx.de>
2022-10-23 18:08:22 +0200

    Add typewriter effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Suspense, useRef, useState } from "react";
+import { useTypewriter } from "react-simple-typewriter";
 import HeaderAnimation from "./HeaderAnimation";
 import Results from "./Results/Results";
 import ResultSkeletons from "./Results/ResultSkeletons";
 
 function App() {
   const [city, setCity] = useState('')
+  const [placeholder] = useTypewriter({ words: ["Nach welcher Stadt suchst du?", "München", "Berlin", "Regensburg"], loop: true })
   const searchInput = useRef<null | HTMLInputElement>(null);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -24,7 +26,7 @@ function App() {
           </h1>
           <p className="max-w-3xl mx-auto text-md text-white">Anwendbar für deutsche Städte.</p>
         </div>
-        <input ref={searchInput} onKeyDown={handleKeyDown} className="rounded-3xl p-3 pl-6 w-80 text-md focus:outline-none" placeholder="Nach welcher Stadt suchst du?"/>
+        <input ref={searchInput} placeholder={placeholder + ""} onKeyDown={handleKeyDown} className="rounded-3xl p-3 pl-6 w-80 text-md focus:outline-none"/>
         <Suspense fallback={<ResultSkeletons/>}>
           <Results city={city} />
         </Suspense>
